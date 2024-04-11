83dc62f82b55fdb5244dc04b222eb1e4fb138d72
Gosha Simakov <yolo73104@gmail.com>
2024-04-11 20:11:36 +0500

    api update

--- app/api/v1/endpoints/color.py
+++ app/api/v1/endpoints/color.py
@@ -4,6 +4,7 @@ from fastapi_pagination import Params
 from app import schemas
 from app import models
 from app import crud
+from app.api.v1 import dependencies as deps
 
 from app.schemas.response_schema import (
     IPostResponseBase,
@@ -17,12 +18,12 @@ from app.schemas.response_schema import (
 router = APIRouter()
 
 obj_in_message = 'Color'
-
 model = models.Color
 read_schema = schemas.IColorRead
 create_schema = schemas.IColorCreate
-
 crud_repo = crud.color
+deps_from_path = deps.get_color_by_id_from_path
+
 
 @router.get("/list")
 async def read_color_list(
@@ -35,53 +36,38 @@ async def read_color_list(
     return create_response(data=response)
 
 
-# @router.get("/{id}")
-# async def get_color_by_id(
-#         current: model = Depends(
-#             deps_from_path
-#         ),
-# ) -> IGetResponseBase[read_schema]:
-#     """
-#     Gets a board by its id
-#     """
-#     return create_response(data=current)
-#
-#
-# @router.put("/{id}")
-# async def update_board_by_id(
-#         update: update_schema,
-#         current: model = Depends(
-#             deps_from_path
-#         ),
-# ) -> IPutResponseBase[read_schema]:
-#     """
-#     Updates a board by id
-#     """
-#
-#     updated = await crud.sensor.update(obj_current=current, obj_new=update)
-#     return create_response(data=updated, message='{} updated'.format(obj_in_message))
-#
-#
-# @router.post("", status_code=status.HTTP_201_CREATED)
-# async def create_board(
-#         create: create_schema
-# ) -> IPostResponseBase[read_schema]:
-#     """
-#     Creates a new board
-#     """
-#     created = await crud_repo.create(obj_in=create)
-#     return create_response(data=created, message='{} created'.format(obj_in_message))
-#
-#
-# @router.delete("/{id}")
-# async def remove_board(
-#         current: model = Depends(
-#             deps_from_path
-#         ),
-# ) -> IDeleteResponseBase[read_schema]:
-#     """
-#     Deletes a board by id
-#     """
-#
-#     deleted = await crud_repo.remove(id=current.id)
-#     return create_response(data=deleted, message="{} removed".format(obj_in_message))
+@router.get("/{id}")
+async def get_color_by_id(
+        current: model = Depends(
+            deps_from_path
+        ),
+) -> IGetResponseBase[read_schema]:
+    """
+    Gets a color measurement by its id
+    """
+    return create_response(data=current)
+
+
+@router.post("", status_code=status.HTTP_201_CREATED)
+async def create_color(
+        create: create_schema
+) -> IPostResponseBase[read_schema]:
+    """
+    Creates a new color measurement
+    """
+    created = await crud_repo.create(obj_in=create)
+    return create_response(data=created, message='{} created'.format(obj_in_message))
+
+
+@router.delete("/{id}")
+async def remove_color(
+        current: model = Depends(
+            deps_from_path
+        ),
+) -> IDeleteResponseBase[read_schema]:
+    """
+    Deletes a color measurement by id
+    """
+
+    deleted = await crud_repo.remove(id=current.id)
+    return create_response(data=deleted, message="{} removed".format(obj_in_message))
