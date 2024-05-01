2d70c0453ca4cfcd3769eae48b6f9987f8fd8428
Gosha Simakov <yolo73104@gmail.com>
2024-05-01 15:30:58 +0500

    refact

---
+++ app/api/graphs/deps/color_deps.py
@@ -0,0 +1,18 @@
+from fastapi import Query
+
+from app import crud
+from app import models
+
+__all__ = ['get_color_multi_by_hardware_id_from_query']
+
+model = models.Color
+crud_repo = crud.color
+HID_param_description = 'Hardware ID of the measurements source'
+limit_param_description = 'Number of measurements from last record"'
+
+
+async def get_color_multi_by_hardware_id_from_query(
+        hardware_id: str = Query(description=HID_param_description),
+        limit: int = Query(default=50, description=limit_param_description)
+) -> list:
+    return await crud_repo.get_multi_by_hardware_id(hardware_id=hardware_id, limit=limit)
